1ad9105ccde96763d53233b67cbb23f22b8ea30e
erimendz_fed36_cinammon <erimendz@gmail.com>
2023-01-20 15:21:47 +0300

    add variables to this script

--- localusers/luser-demo01.sh
+++ localusers/luser-demo01.sh
@@ -3,3 +3,29 @@
 # This script displays various information to the screen
 
 echo 'Hello'
+# add a constant variable big case
+WORD='script'
+# call that variable
+echo "This is a shell $WORD"
+
+# demonstrate that single quotes do not do shell expansion
+echo '$WORD'
+
+# use another alternative syntax
+echo "Another syntax using brace expansion: This is a shell ${WORD}"
+# append text to the variable using brace expansion
+echo "${WORD}ing is fun!"
+# this doesnt work
+echo "$WORDing is fun! ( doesnt work )"
+# new variable
+ENDING='ed'
+
+# combine the two variables aka concatenate
+echo "This is ${WORD}${ENDING}."
+
+# variable re-assignment
+ENDING='ing'
+echo "${WORD}${ENDING} is fun!."
+
+# another reassignment
+echo "You are going to write many ${WORD}s in this class!"
